248272a35ddedc6e7f3c36652584320ec0fc8428
Felice Forby <feliceforby@gmail.com>
2018-04-23 22:53:38 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,13 +10,13 @@ end
 
 def monopoly
 	monopoly = {
-		:railroads => {}
+		railroads: {}
 	}
 end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads => {
+		railroads: {
 			pieces: 4
 		}
 	}
